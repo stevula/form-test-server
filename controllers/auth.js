257b57faf32847235904f16a425047e0b18a5826
stevula <brodericksteven@gmail.com>
2017-12-30 17:04:46 -0800

    Add jwt tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,26 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config.js');
+
+const tokenForUser = (user) => {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+};
 
 exports.signup = (req, res, next) => {
   const { email, password } = req.body;
 
-  User.findOne({ email })
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Email and password are required.' });
+  }
+
+  return User.findOne({ email })
     .then((existingUser) => {
       if (existingUser) {
         return res.status(422).send({ error: 'Email already taken.' });
       }
-      return new User({ email, password }).save().then(() => res.json({
-        success: true,
+      return new User({ email, password }).save().then(user => res.json({
+        token: tokenForUser(user),
       }));
     })
     .catch(error => next(error));
--- models/user.js
+++ models/user.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String,
+});
+
+userSchema.pre('save', function encryptPassword(next) {
+  bcrypt.genSalt(10, (error, salt) => {
+    if (error) return next(error);
+
+    return bcrypt.hash(this.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      return next();
+    });
+  });
 });
 
 const User = mongoose.model('user', userSchema);
