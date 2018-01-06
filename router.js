d9af05ffe7eee528e9a86a58cbc2f510e353f1ca
stevula <brodericksteven@gmail.com>
2018-01-05 17:59:52 -0800

    Reenable auth middleware

--- router.js
+++ router.js
@@ -8,8 +8,8 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = (app) => {
-  app.get('/questions', /*requireAuth, */questionsController.index);
-  app.patch('/answers', /*requireAuth, */answersController.update);
+  app.get('/questions', requireAuth, questionsController.index);
+  app.patch('/answers', requireAuth, answersController.update);
   app.post('/signup', authController.signup);
   app.post('/signin', requireSignin, authController.signin);
 };
